c443b01d939806f19821aecf369e0327afb2f01e
Alexander Morozov <gelu.speculum@gmail.com>
2021-04-19 10:36:24 +0300

    Add MT test, update Board interface and add static analysis

--- src/test_board.cpp
+++ src/test_board.cpp
@@ -3,6 +3,8 @@
 #include <gtest/gtest.h>
 
 #include <array>
+#include <list>
+#include <thread>
 
 namespace {
 
@@ -226,6 +228,26 @@ const std::vector<Nineteen> nineteens = {
     , {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 0, true, true,  0,  0}
 };
 
+unsigned calc_hamming(const Board & b)
+{
+    unsigned hamming = 0, expected = 0;
+    const unsigned max = i > 0 ? i * i - 1 : 0;
+    for (unsigned x = 0; x < i; ++x) {
+        for (unsigned y = 0; y < i; ++y) {
+            if (expected < max) {
+                ++expected;
+            }
+            else {
+                expected = 0;
+            }
+            if (b[x][y] != expected) {
+                ++hamming;
+            }
+        }
+    }
+    return hamming;
+}
+
 } // anonymous namespace
 
 TEST(BoardTest, empty)
@@ -246,7 +268,8 @@ TEST(BoardTest, empty)
         EXPECT_TRUE(copy.is_solvable());
     }
 
-    Board bb(0), other(11);
+    const auto bb = Board::create_random(0);
+    auto other = Board::create_random(11);
     EXPECT_EQ(0, bb.size());
     EXPECT_TRUE(bb.is_goal());
     EXPECT_EQ(0, bb.hamming());
@@ -263,7 +286,7 @@ TEST(BoardTest, empty)
 
 TEST(BoardTest, singleton)
 {
-    Board b(1);
+    const auto b = Board::create_random(1);
     EXPECT_EQ(1, b.size());
     EXPECT_TRUE(b.is_goal());
     EXPECT_EQ(0, b.hamming());
@@ -274,8 +297,9 @@ TEST(BoardTest, singleton)
 
 TEST(BoardTest, copy)
 {
-    Board b(3), bb = b;
-    Board bbb(7);
+    const auto b = Board::create_random(3);
+    const auto bb = b;
+    auto bbb = Board::create_random(11);
     bbb = b;
     EXPECT_EQ(b, bb);
     EXPECT_EQ(bb, bbb);
@@ -288,23 +312,9 @@ TEST(BoardTest, copy)
 TEST(BoardTest, random_create)
 {
     for (unsigned i = 0; i < 111; ++i) {
-        Board b(i);
+        const auto b = Board::create_random(i);
         EXPECT_EQ(i, b.size());
-        unsigned hamming = 0, expected = 0;
-        const unsigned max = i > 0 ? i * i - 1 : 0;
-        for (unsigned x = 0; x < i; ++x) {
-            for (unsigned y = 0; y < i; ++y) {
-                if (expected < max) {
-                    ++expected;
-                }
-                else {
-                    expected = 0;
-                }
-                if (b[x][y] != expected) {
-                    ++hamming;
-                }
-            }
-        }
+        const auto hamming = calc_hamming(b);
         EXPECT_EQ(hamming, b.hamming()) << " for " << i;
         EXPECT_EQ(hamming == 0, b.is_goal()) << " for " << i;
     }
@@ -319,7 +329,7 @@ TEST(BoardTest, two)
         EXPECT_EQ(c.solvable, b.is_solvable()) << "Wrong solvability detection for" << c;
         EXPECT_EQ(c.hamming, b.hamming()) << "Wrong hamming distance for " << c;
         EXPECT_EQ(c.manhattan, b.manhattan()) << "Wrong manhattan distance for" << c;
-        Board other(7);
+        auto other = Board::create_random(7);
         other = b;
         EXPECT_EQ(b.size(), other.size());
         EXPECT_EQ(b.is_goal(), other.is_goal());
@@ -387,4 +397,57 @@ TEST(BoardTest, nineteens)
         EXPECT_EQ(c.hamming, b.hamming()) << "Wrong hamming distance for " << c;
         EXPECT_EQ(c.manhattan, b.manhattan()) << "Wrong manhattan distance for" << c;
     }
+}
+
+TEST(BoardTest, parallel)
+{
+    struct Result
+    {
+        unsigned n = 0;
+        unsigned size = 0;
+        unsigned hamming = 0;
+        unsigned manhattan = 0;
+        bool is_goal = false;
+        bool is_solvable = false;
+    };
+    const unsigned max = 101;
+    const unsigned mul = 5;
+    std::vector<std::thread> threads;
+    std::list<Result> results;
+    threads.reserve(max);
+    std::mutex mutex;
+    for (unsigned i = 0; i < mul * max; ++i) {
+        threads.emplace_back([i, &mutex, &results] () {
+                const auto n = i / mul;
+                Board b;
+                {
+                    std::lock_guard lock(mutex);
+                    b = Board::create_random(n);
+                }
+                Result res;
+                res.n = n;
+                res.size = b.size();
+                res.hamming = b.hamming();
+                res.manhattan = b.manhattan();
+                res.is_goal = b.is_goal();
+                res.is_solvable = b.is_solvable();
+                std::lock_guard lock(mutex);
+                results.emplace_back(res);
+            });
+    }
+    for (auto & t : threads) {
+        t.join();
+    }
+    std::size_t total_solvable = 0;
+    for (const auto & res : results) {
+        EXPECT_EQ(res.n, res.size) << "Inconsistent board size";
+        const auto hamming = calc_hamming(b);
+        EXPECT_EQ(hamming, res.hamming) << "For board with size " << res.n;
+        EXPECT_EQ(hamming == 0, res.is_goal) << "For board with size " << res.n;
+        EXPECT_EQ(res.is_goal, res.manhattan == 0) << "For board with size " << res.n;
+        if (res.is_solvable) {
+            ++total_solvable;
+        }
+    }
+    EXPECT_GE(total_solvable, mul * 2) << "At least all empty and singleton boards should be solvable";
 }
